b26e65fcc707b32a6e3350a831a1bf42a980638d
Ikaros <327209194@qq.com>
2022-06-02 16:59:31 +0800

    更新基础功能，发布v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-cordova plugin add cordova-plugin-file
-cordova plugin add cordova-plugin-media
+# 前言
+
+本项目基于Cordova开发，打包的apk支持Android9+，主要功能为 监听b站用户直播情况，开播进行闹钟提示
+
+## 使用介绍
+
+## 效果图
+
+## 插件安装
+
+cordova plugin add cordova-plugin-file  
+cordova plugin add cordova-plugin-media  
--- www/js/index.js
+++ www/js/index.js
@@ -20,15 +20,125 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 
-var Interval1;
+var live_status_interval;
+var clear_log_interval;
 var fileURL = "";
+var uids = [];
+var room_ids = [];
+var usernames = [];
+
+// 睡眠 毫秒
+function sleep(delay) {
+    let start = +new Date()
+    while (+new Date() - start < delay) {}
+}
+
+// 更新uid配置
+function update_uid_config() {
+    var uid_input = document.getElementById("uid_input").value;
+    uids = uid_input.split(" ");
+    log(uids);
+    for(var i = 0; i < uids.length; i++) {
+        print_username(i);
+        sleep(100);
+        get_room_id(i);
+        sleep(100);
+    }
+}
+
+// 输出uid对应用户昵称
+function print_username(index) {
+    var username = "-";
+    // 构建url
+    var url = "https://api.bilibili.com/x/space/acc/info?mid=" + uids[index] + "&jsonp=jsonp";
+    // 建立所需的对象
+    var httpRequest = new XMLHttpRequest();
+    // 打开连接  将请求参数写在url中 
+    httpRequest.open('GET', url, true);
+    // 发送请求  将请求参数写在URL中
+    httpRequest.send();
+    // 获取数据后的处理程序
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            // 获取到json字符串
+            var ret = httpRequest.responseText;
+            // 转为JSON对象
+            var json = JSON.parse(ret);
+            // log(json);
+            // alert(json);
+
+            if (json.length == 0) {
+                log(uids[index] + "无数据", "error"); 
+                usernames.push(username);
+                return;
+            }
+
+            if(json["code"] != 0) {
+                log(uids[index] + "获取用户昵称失败，请检查uid是否正确", "error");
+                usernames.push(username);
+                return;
+            }
+
+            username = json["data"]["name"];
+            log("用户名:" + username + " UID:" + uids[index]);
+            usernames.push(username);
+        }
+    };
+}
+
+// 通过uid获取room_id
+function get_room_id(index) {
+    var room_id = "";
+    // 构建url
+    var url = "https://api.live.bilibili.com/room/v2/Room/room_id_by_uid?uid=" + uids[index]; 
+    // 建立所需的对象
+    var httpRequest = new XMLHttpRequest();
+    // 打开连接  将请求参数写在url中 
+    httpRequest.open('GET', url, true);
+    // 发送请求  将请求参数写在URL中
+    httpRequest.send();
+    // 获取数据后的处理程序
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            // 获取到json字符串
+            var ret = httpRequest.responseText;
+            // 转为JSON对象
+            var json = JSON.parse(ret);
+            // log(json);
+            // alert(json);
+
+            if (json.length == 0) {
+                log(uids[index] + "无数据", "error"); 
+                room_ids.push(room_id);
+                return;
+            }
+
+            if(json["code"] != 0) {
+                log(uids[index] + "获取房间号失败，请检查uid是否正确", "error");
+                room_ids.push(room_id);
+                return;
+            }
+
+            room_id = json["data"]["room_id"];
+            log("房间号:" + room_id + " UID:" + uids[index]);
+            room_ids.push(room_id);
+        }
+    };
+}
+
+// 开启循环监听live_status
+function loop_listen_live_status() {
+    for(var i = 0; i < room_ids.length; i++) {
+        get_live_status(i);
+        sleep(100);
+    }
+}
 
 // 获取直播间开播情况
-function get_live_status() {
+function get_live_status(index) {
     var live_status = 0;
-    var room_id = document.getElementById("room_id").value;
     // 构建url
-    var url = "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id="  + room_id 
+    var url = "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id="  + room_ids[index];
     //    + "&protocol=0,1&format=0,1,2&codec=0,1";
     // 建立所需的对象
     var httpRequest = new XMLHttpRequest();
@@ -44,13 +154,11 @@ function get_live_status() {
             //console.log(ret);
             // 转为JSON对象
             var json = JSON.parse(ret);
-            // console.log(json);
             // log(json);
             // alert(json);
 
             if (json.length == 0) {
-                // console.log("无数据"); 
-                // log("无数据"); 
+                log(room_ids[index] + "无数据", "error"); 
                 return 0;
             }
 
@@ -60,14 +168,15 @@ function get_live_status() {
             if(live_status == 1) {
                 var audio = document.getElementById('audio_id');
                 audio.play();
-                clearInterval(Interval1);
+                log(usernames[index] + " 正在直播中！！！");
+                clearInterval(live_status_interval);
+                log("闹钟停止运行", "success");
             }
 
             return live_status;
         }
     };
 
-    // console.log("get_live_status() over");
     // log("get_live_status() over");
 
     return 0;
@@ -97,23 +206,44 @@ function playAudio(url) {
 
 // 自启动运行
 function self_start_run() {
-    console.log("自启动运行还未实现");
     log("自启动运行还未实现", "error");
-    // console.log("self_start_run() over");
-    // log("self_start_run() over");
 }
 
 // 普通运行
 function normal_run() {
-    clearInterval(Interval1);
+    update_uid_config();
+    clearInterval(live_status_interval);
     // get_live_status();
-    Interval1 = setInterval(function(){get_live_status()}, (10000 + get_random(0, 100)));
-    console.log("普通运行中...");
+    var loop_interval = parseInt(document.getElementById("loop_interval").value);
+    live_status_interval = setInterval(function(){loop_listen_live_status()}, (loop_interval * 1000 + get_random(0, 100)));
     log("普通运行中...", "success");
 }
 
+// 停止运行
+function stop_run() {
+    clearInterval(live_status_interval);
+    var audio = document.getElementById('audio_id');
+    audio.stop();
+    log("已停止运行", "success");
+}
+
 document.addEventListener('deviceready', function () {
-    document.getElementById("room_id").value = "23720185";
+    document.getElementById("uid_input").value = "3709626 1944648347";
+    document.getElementById("loop_interval").value = "5";
+
+    // 功能按钮
+    var func_btn = document.getElementById('func_btn');
+    func_btn.onclick = function(){
+        document.getElementById("func_div").style.display = "block";
+        document.getElementById("set_div").style.display = "none";
+    };
+
+    // 设置按钮
+    var set_btn = document.getElementById('set_btn');
+    set_btn.onclick = function() {
+        document.getElementById("func_div").style.display = "none";
+        document.getElementById("set_div").style.display = "block";
+    };
 });
 
 //取得[n,m]范围随机数
@@ -133,7 +263,7 @@ function log(msg, level) {
         msg = JSON.stringify(msg, null, "  ");
     }
 
-    console.log(msg);
+    // console.log(msg);
 
     if (level === "status" || level === "error" || level === "success") {
         var msgDiv = document.createElement("div");
@@ -160,12 +290,31 @@ function log(msg, level) {
     }
 }
 
+// 清空日志
+function clear_log() {
+    document.getElementById("output").innerHTML = "";
+    clearInterval(clear_log_interval);
+    log("已停止每分钟清空日志", "success");
+}
+
+// 每分钟清空日志
+function auto_clear_log() {
+    clear_log_interval = setInterval(function(){document.getElementById("output").innerHTML = "";}, 60000);
+}
+
+// 选择音频文件
 function set_audio(node) {
     // var file = document.getElementById('file').files[0];
     var url = URL.createObjectURL(node.files[0]);
-    console.log(url);
     log("audio_url:" + url);
     document.getElementById("audio_id").src = url;
+}
+
+// 选择背景图片
+function set_bg(node) {
+    var url = URL.createObjectURL(node.files[0]);
+    log("img_url:" + url);
+    document.body.style.backgroundImage = 'url(' + url + ')';
 }
 
 function setFileURL(node) {
